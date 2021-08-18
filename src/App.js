50233ac4a017e1980939111d706c8a204dc1bcba
Alfisahr <alfisahr@gmail.com>
2021-08-18 16:25:06 +0700

    Refactoring component structure, move all searchLookup.jsx into app.js, then remove the file.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
-import SearchLookup from './components/searchLookup';
-// import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { getUserData } from "./utils/api";
+import UserResult from './components/userResult'
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-    <SearchLookup />
-    </div>
-  );
+  const [username, setUsername] = useState(null);
+  const [userResult, setUserResult] = useState(null)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getUserData(username).then((user) => {
+      setUserResult(user)
+    });
+  };
+
+  if (userResult !== null) {
+    return <UserResult userResult={userResult} />;
+  } else {
+    return (
+      <div className="App">
+        <div className="content-wrapper">
+          <div className="app-title">
+            <span className="main">GitHub</span>{" "}
+            <span className="secondary">User Lookup</span>
+          </div>
+          <div className="search-wrapper">
+            <form onSubmit={handleSubmit}>
+              <input
+                type="text"
+                name="search"
+                placeholder="Github username..."
+                onChange={(e) => setUsername(e.target.value)}
+                value={username !== null ? username : ""}
+                id="search"
+              />
+              <button type="submit" name="submit">
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/userResult.jsx
@@ -0,0 +1,11 @@
+const UserResult = ({userResult}) => {
+	console.log(userResult)
+	return (
+		<div className="container">
+			<div className="sidebar">ini sidebar</div>
+			<div className="content">ini kontent</div>
+		</div>
+	);
+};
+
+export default UserResult;
